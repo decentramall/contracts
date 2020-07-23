33be4dd8a1ef4c34d747a2edf4a6e6f8543920b2
Bernardo Vieira <bernardo5@live.com.pt>
2020-07-23 22:36:08 +0100

    ignore migrations on dev

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -5,19 +5,21 @@ const RentalAgent = artifacts.require("RentalAgent");
 const agent = '0xfe54a4bf10e879fc18b0facc4c8c3c72f9e082c5'; //ONE OF YOUR LOCAL BLOCKCHAIN ADDRESS
 
 module.exports = function (deployer, network, accounts) {
-  let admin = accounts[0];
-  deployer.then(async () => {
-    //Always deploy EstateAgent first
-    await deployer.deploy(EstateAgent, 100, 1);
+  if (network !== 'development' && network !== 'coverage') {
+    let admin = accounts[0];
+    deployer.then(async () => {
+      //Always deploy EstateAgent first
+      await deployer.deploy(EstateAgent, 100, 1);
 
-    //Wait till deployed
-    this.estateAgent = await EstateAgent.deployed();
+      //Wait till deployed
+      this.estateAgent = await EstateAgent.deployed();
 
-    //Get the address of the deployed token contract within EstateAgent
-    let tokenAddress = await this.estateAgent.token.call({ from: admin });
-    this.token = await DecentramallToken.at(tokenAddress);
+      //Get the address of the deployed token contract within EstateAgent
+      let tokenAddress = await this.estateAgent.token.call({ from: admin });
+      this.token = await DecentramallToken.at(tokenAddress);
 
-    //Use both the address in RentalAgent contract
-    await deployer.deploy(RentalAgent, this.token.address, EstateAgent.address);
-  });
+      //Use both the address in RentalAgent contract
+      await deployer.deploy(RentalAgent, this.token.address, EstateAgent.address);
+    });
+  }
 };
