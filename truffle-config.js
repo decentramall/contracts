07bae95c856ead402208361f2fc1d1d8f7fd0b53
Bernardo Vieira <bernardo5@live.com.pt>
2020-07-28 11:37:56 +0100

    draft load space tokens

--- truffle-config.js
+++ truffle-config.js
@@ -51,6 +51,12 @@ module.exports = {
     test: {
       host: '127.0.0.1', // Localhost (default: none)
       port: 7545, // Standard Ethereum port (default: none)
+      network_id: '5777', // Any network (default: none)
+    },
+
+    test: {
+      host: '127.0.0.1', // Localhost (default: none)
+      port: 7545, // Standard Ethereum port (default: none)
       network_id: '*', // Any network (default: none)
     },
 
