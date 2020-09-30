32b349f949a49b04b4e4522b50f35cd001311397
Eason Chai <echai2905@gmail.com>
2020-09-30 23:49:35 +0800

    Fixed generating types

--- contracts/test/DAI.sol
+++ contracts/test/DAI.sol
@@ -6,7 +6,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 contract DAI is ERC20 {
     constructor(address[] memory accounts) public ERC20("DAI", "DAI") {
         for (uint256 i = 0; i < accounts.length; i++) {
-            _mint(accounts[i], 5000 * 10**18);
+            _mint(accounts[i], 500 * 10**18);
         }
     }
 }
