653c0fb74cfd4db1b877f3b96aa65b8456f00090
Eason Chai <echai2905@gmail.com>
2020-10-14 11:51:00 +0800

    create new contract

--- contracts/DecentramallRegistry.sol
+++ contracts/DecentramallRegistry.sol
@@ -1,10 +1,26 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.6.8;
 
-/**
-This contract will house the renting, registry and buying of SPACE
-*/
+import Decentramall from './Decentramall.sol';
+
+/** @title Decentramall SPACE token
+ * @notice This contract will house the renting, registry and buying of SPACE
+ */
 
 contract DecentramallRegistry {
-    
+    // DAI contract address
+    address public dai;
+    //Multiplier to get price in 18 decimals
+    uint256 public multiplier = 1000000000000000000;
+
+    struct SpaceDetails {
+        address owner;
+        address renter;
+        uint256 rentalEarned;
+        uint256 expiryBlock;
+    }
+
+    function buy() public {
+        
+    }
 }
