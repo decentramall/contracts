3e2c00b8a94dea2669e9f615cff89047c1dad764
Eason Chai <echai2905@gmail.com>
2020-10-16 21:47:16 +0800

    Cleanup

--- scripts/quicktest.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-sleep 5 && truffle deploy --network development --reset &
-npx ganache-cli --mnemonic "drama obscure around omit goose hammer ostrich gift inner dress talent expand" --gasLimit 0xfffffffffff --networkId 1337 --port 8545
-
-
